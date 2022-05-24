bd0e12801f955fa0075441d9c81a43136371f9fd
TitoGrossi <tito.brossi@gmail.com>
2022-05-24 10:54:52 -0300

    Fix error message on test

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -16,7 +16,7 @@ describe("Product unit tests", () => {
     it("should throw error when price is less than zero", () => {
         expect(() => {
             const product = new Product("123", "", -1)
-        }).toThrowError("product: Price must be greater than or equal to 0")
+        }).toThrowError("product: Name is required, product: Price must be greater than or equal to 0")
     })
 
     it("should change name", () => {
